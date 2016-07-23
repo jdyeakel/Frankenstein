667ea35a114bd74af2e3d295577b1d03a51f944e
jdyeakel <jdyeakel@gmail.com>
2016-07-22 19:21:57 -0600

    julia code

--- CompetitionSims.jl
+++ CompetitionSims.jl
@@ -1,4 +1,5 @@
-Using ODE
+using ODE
+using Gadfly
 
 a_mh = 0.5;
 a_hm = 0.5;
@@ -7,8 +8,10 @@ k_h = 100;
 r_h = 0.5;
 r_m = 0.5;
 
+initial = [20.0,10.0];
 
-
+include("/home/jdyeakel/Dropbox/PostDoc/2016_Frankenstein/src/frank_comp.jl")
 T, den = ode23(frank_comp, initial, [0., 40]);
 den = hcat(den...)';
 
+plot(x=T,y=den[:,1],Geom.line)
