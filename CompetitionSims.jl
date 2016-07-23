42e3fbcfb9967d3562df5f70e5dd458adf73a4f7
jdyeakel <jdyeakel@gmail.com>
2016-07-23 16:14:23 -0600

    extinction analysis in Julia

--- CompetitionSims.jl
+++ CompetitionSims.jl
@@ -1,5 +1,6 @@
 using ODE
 using Gadfly
+include("/home/jdyeakel/Dropbox/PostDoc/2016_Frankenstein/src/frank_comp.jl")
 
 a_hm = 1.5;
 a_mh = a_hm/2;
@@ -9,7 +10,6 @@ r_m = 0.0067;
 
 initial = [1.09*10^9,1.0];
 
-include("/home/jdyeakel/Dropbox/PostDoc/2016_Frankenstein/src/frank_comp.jl")
 T, den = ode45(frank_comp, initial, [0., 10.0^7]);
 den = hcat(den...)';
 
@@ -19,4 +19,38 @@ plot(
   Scale.y_log10
   )
 
-for i=1:
+########################
+
+avec = collect(1.0:0.1:10.0);
+lavec = length(avec);
+cvec = collect(1.0:1:10.0)
+lcvec = length(cvec);
+t_ext = zeros(lavec,lcvec);
+for j=1:lcvec
+  for i=1:lavec
+    #Initialize parameters
+    a_hm = avec[i];
+    c = cvec[j];
+    a_mh = a_hm/c;
+    k = 10*10^9;
+    r_h = 0.0067;
+    r_m = 0.0067;
+    thresh = 1000.0;
+    initial = [1.09*10^9,1.0];
+
+    T, den = ode45(frank_comp, initial, [0., 10.0^7]);
+    den = hcat(den...)';
+
+    #When does the human trajectory fall below threshold value?
+    hden = den[:,1];
+    pos = find(x->x<thresh,hden);
+    if length(pos) > 0
+      t_ext[i,j] = T[pos[1]];
+    else
+      t_ext[i,j] = Inf;
+    end
+  end
+end
+
+
+plot([layer(y=t_ext[:,j],x=avec, Geom.line) for j in 1:lcvec]...,Coord.Cartesian(xmin=1, xmax=10))
--- src/frank_comp.jl
+++ src/frank_comp.jl
@@ -1,6 +1,6 @@
 function frank_comp(t, y)
 	[
-	r_h*y[1]*(1 - (a_mh*y[2] + y[1])/k);
-	r_m*y[2]*(1 - (a_hm*y[1] + y[2])/k);
+	r_h*y[1]*(1 - (a_hm*y[2] + y[1])/k);
+	r_m*y[2]*(1 - (a_mh*y[1] + y[2])/k);
 	]
 end
